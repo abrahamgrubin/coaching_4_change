7d7cb58a656caf6e6afb99060e8c7f87e92ba796
Abraham Rubin <abrahamgrubin@gmail.com>
2014-12-02 17:46:48 -0500

    Added style to the donations page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,20 +4,20 @@ class ChargesController < ApplicationController
 
   def create
   # Amount in cents
-    @amount = 500
+    @amount = 1000
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
+      :card  => params[:stripeToken],
     )
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :description => 'Donate to C4C',
       :currency    => 'usd'
     )
-
+  flash[:success] = "Thank you for supporting Coaching 4 Change!"
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
