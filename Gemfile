f64c8d44befe0bc8593860b5fd02dad143bc7dbd
Abraham Rubin <abrahamgrubin@gmail.com>
2014-12-03 17:45:29 -0500

    Messed around with the mailer

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'high_voltage', '~> 2.2.1'
 #gem for holding static pages 
 gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 
+gem "figaro"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,5 @@
 class ContactsController < ApplicationController
 
-def show 
-  @contact = Contact.find(params[:id])
-end
-
 def new 
   @contact = Contact.new
 end 
@@ -11,7 +7,7 @@ end
 def create 
   @contact = Contact.new(contact_params)
   if @contact.save 
-    ContactMailer.contact_confirmation.deliver
+    ContactMailer.contact_confirmation(@contact).deliver
     redirect_to 'new', notice: "You have contacted C4C successfully. You will recieve a confirmation email within 2 business days."
   else 
     render :new 
